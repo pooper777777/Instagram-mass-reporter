ec69d29b9e46249539a7c3cc1857eccb8e4cd9bc
pooper777777 <106491781+pooper777777@users.noreply.github.com>
2022-05-30 00:01:55 +0400

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -23,7 +23,7 @@ username = args.username
 acc_file = args.file
 
 if username == "" :
-	username = input("Username: ")
+	username = input("o.x_m.ar")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
